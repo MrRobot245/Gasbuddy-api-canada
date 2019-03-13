a702cbdeebd556095f64dce2edb624ba6dcae4a0
Nathan Reymer <nathan@mmddata.ca>
2019-03-13 16:06:23 -0400

    Working

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ var app = express();
 const asyncHandler = require('express-async-handler')
 
 
-app.get('/location/:gps', asyncHandler(async (req, res, next) => {
-	getGasPrice(req.params.gps, function (result) {
+app.get('/location/:term', asyncHandler(async (req, res, next) => {
+	getGasPrice(req.params.term, function (result) {
 		res.send(({
 			"status": 200,
 			"error": null,
@@ -21,55 +21,40 @@ module.exports = app;
 
 
 
-function getGasPrice(gps, callback) {
+function getGasPrice(term, callback) {
 	(async () => {
 		const browser = await puppeteer.launch({
 			args: ['--no-sandbox', '--disable-setuid-sandbox']
 		});
 		const page = await browser.newPage();
 		// console.log(gps);
-		var latLong = gps.split("&");
-		// console.log(latLong[0]);
-		await page.goto("https://www.gasbuddy.com/home?lat=" + latLong[0] + "&lng=" + latLong[1] + "&fuel=1");
+		var input = term.split("&");
+		if(input.length==2){
+			await page.goto("https://www.gasbuddy.com/home?lat=" + input[0] + "&lng=" + input[1] + "&fuel=1");
+		}
+		else{
+			await page.goto("https://www.gasbuddy.com/home?search=" +encodeURI(term)+ "&fuel=1");
+		}
+		
 
 		let content = await page.content();
 		var $ = cheerio.load(content);
 		var gasArray = new Array();
 
-		$('div[class*=styles__stationList]').each(function (i, element) {
+		$('div[class*=GenericStationList__stationListItem]').each(function (i, element) {
 			let type = $(this).find('h3').text();
 			let location = $(this).find('h3').next().next().html();
-			let price = $(this).find('span[class*=styles__price]').text();
-			let time = $(this).find('span[class*=styles__postedTime]').text();
+			let price = $(this).find('span[class*=GenericStationList__price___]').text();
+			let time = $(this).find('span[class*=style__postedTime___]').text();
 			gasArray.push({
 				'price': price,
 				'location': location,
 				'time': time,
 				'type': type
 			});
-
 		});
-		////
-		////Town basaed
-		////
 
-		// $('.prices-table tr').each(function (i, element) {
-		// 	if ($(this).find('.gb-price').html() != null) {
-		// 		let price = $(this).find('.gb-price').text().replace(/\s/g, '');
-		// 		let location = $(this).find('strong').text().replace(/\s/g, '');
-		// 		let time = $(this).find('.date').text().replace(/\s/g, '');
-		// 		time = time.substring(0, time.length - 3);
-		// 		let locationAddr = $(this).find('.visible-xs').prev().text();
-		// 		// console.log(locationAddr);
-		// 		gasArray.push({
-		// 			'price': price,
-		// 			'locaiton': location,
-		// 			'time': time,
-		// 			'addr': locationAddr
-		// 		});
-		// 		// console.log(price);
-		// 	}
-		// });
+		gasArray.pop();
 		// console.log(gasArray);
 		callback(gasArray);
 		await browser.close();
